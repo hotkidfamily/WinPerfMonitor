a4b674516f07cf1bc5ad9341206eb2908ca328a1
yanli <yanli@yy.com>
2023-09-26 19:38:03 +0800

    get pid

--- Perfmon/MainForm.cs
+++ Perfmon/MainForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,10 +16,14 @@ namespace Perfmon
 {
     public partial class MainForm : Form
     {
-        public PerformanceCounter? cpuProc;
-        public PerformanceCounter? cpuTotal;
-        public PerformanceCounter? ramAva;
-        public PerformanceCounter? ramUsed;
+        private PerformanceCounter? cpuProc;
+        private PerformanceCounter? cpuTotal;
+        private PerformanceCounter? ramAva;
+        private PerformanceCounter? ramUsed;
+
+        private uint pid = 0;
+
+
         public MainForm()
         {
             InitializeComponent();
@@ -28,6 +33,12 @@ namespace Perfmon
             ramUsed = new PerformanceCounter("Memory", "Committed Bytes");
             update();
         }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(Point pt);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
         private void btnShotProcess_Click(object sender, EventArgs e)
         {
@@ -45,6 +56,9 @@ namespace Perfmon
 
             Point v = e.Location;
 
+            var Handle = WindowFromPoint(v);
+            GetWindowThreadProcessId(Handle, out pid);
+
         }
 
         async Task update()
@@ -54,7 +68,7 @@ namespace Perfmon
                 await Task.Delay(TimeSpan.FromSeconds(1));
                 var v1 = $"{cpuTotal.NextValue():F2}" + "% | ";
                 var v2 = $"{(ramUsed.NextValue() + ramAva.NextValue()) / 1000 / 1000:F2}" + "MB | ";
-                var v3 = $"{ramAva.NextValue() / 1000 / 1000:F2}" + "MB ";
+                var v3 = $"{ramAva.NextValue() / 1000 / 1000:F2}" + "MB | " + $"{pid}";
                 labelCpuAndMem.Text = v1 + v2 + v3;
             }
         }
