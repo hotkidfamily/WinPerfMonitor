6f9be1d01a096aa371372792039979535d520b54
yanli <yanli@yy.com>
2023-09-27 19:35:12 +0800

    init base

--- Perfmon/MainForm.cs
+++ Perfmon/MainForm.cs
@@ -11,50 +11,43 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 using Windows.Win32;
-using System.Net.Sockets;
+using System.Management;
+
 
 namespace Perfmon
 {
     public partial class MainForm : Form
     {
-        private readonly PerformanceCounter? cpuProc;
         private readonly PerformanceCounter? cpuTotal;
         private readonly PerformanceCounter? ramAva;
         private readonly PerformanceCounter? ramUsed;
 
-        class ProcListItem
+        class RunStatusItem
         {
-            public uint pid;
-            public string? procName;
-            public float cpu;
-            public float vMem;
-            public float phyMem;
-            public float totalMem;
-            public float downLink;
-            public float upLink;
-            public float totoalLinkFlow;
-            public uint excuteStatus;
+            public uint pid = 0;
+            public string procName = "undefined";
+            public float cpu = 0;
+            public float vMem = 0;
+            public float phyMem = 0;
+            public float totalMem = 0;
+            public float downLink = 0;
+            public float upLink = 0;
+            public float totoalLinkFlow = 0;
+            public uint excuteStatus = 0;
         }
 
-        private readonly ProcListItem _mproc = new();
+        private readonly RunStatusItem _mproc = new();
+        private static int _phyMemTotal = 0;
+
+        
         public MainForm()
         {
             InitializeComponent();
-            cpuProc = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             cpuTotal = new PerformanceCounter("Processor", "% Processor Time", "_Total");
             ramAva = new PerformanceCounter("Memory", "Available Bytes");
             ramUsed = new PerformanceCounter("Memory", "Committed Bytes");
             _ = MachineUpdate();
         }
-
-        /*        [DllImport("user32.dll")]
-                static extern IntPtr WindowFromPoint(Point pt);
-
-                [DllImport("user32.dll", SetLastError = true)]
-                static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
-
-                [DllImport("user32.dll")]
-                static extern bool GetCursorPos(out Point lpPoint);*/
 
         private void BtnShotProcess_MouseDown(object sender, MouseEventArgs e)
         {
@@ -65,7 +58,7 @@ namespace Perfmon
         {
             PInvoke.GetCursorPos(out Point v);
             var Handle = PInvoke.WindowFromPoint(v);
-            uint pid;
+            uint pid = 0;
             unsafe
             {
                 _ = PInvoke.GetWindowThreadProcessId(Handle, &pid);
@@ -113,13 +106,23 @@ namespace Perfmon
 
         async Task MachineUpdate()
         {
+            _phyMemTotal = GetPhisicalMemory();
+            var core = Environment.ProcessorCount;
+            var mnam = Environment.MachineName;
+            var os = Environment.OSVersion.Version.ToString();
+            Process curProcess = Process.GetCurrentProcess();
+
             while (!IsDisposed)
             {
                 await Task.Delay(TimeSpan.FromSeconds(1));
-                var v1 = $"{cpuTotal.NextValue():F2}" + "% | ";
-                var v2 = $"{(ramUsed.NextValue() + ramAva.NextValue()) / 1000 / 1000:F2}" + "MB | ";
-                var v3 = $"{ramAva.NextValue() / 1000 / 1000:F2}" + "MB | " + $"{_mproc.pid},{_mproc.procName}";
-                labelCpuAndMem.Text = v1 + v2 + v3;
+                StringBuilder sb = new();
+                int rama = (int)((long)Math.Round(ramAva.NextValue()) >> 20);
+                int ram = (int)((long)ramUsed.NextValue() >> 20) + rama;
+
+                sb.Append($"{cpuTotal?.NextValue():F2} % | {mnam} | {os} | {core} | ");
+                sb.Append($"{ram} MB | {rama} MB | {_phyMemTotal} MB | {curProcess.Id},{curProcess.ProcessName}");
+
+                labelCpuAndMem.Text = sb.ToString();
             }
         }
 
@@ -159,6 +162,32 @@ namespace Perfmon
         private void btnBreak_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private static int GetPhisicalMemory()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(); //用于查询一些如系统信息的管理对象 
+            searcher.Query = new SelectQuery("Win32_PhysicalMemory ", "", new[] { "Capacity" }); //设置查询条件 
+            ManagementObjectCollection collection = searcher.Get(); //获取内存容量 
+            ManagementObjectCollection.ManagementObjectEnumerator em = collection.GetEnumerator();
+
+            long capacity = 0;
+            while (em.MoveNext())
+            {
+                ManagementBaseObject baseObj = em.Current;
+                if (baseObj.Properties["Capacity"].Value != null)
+                {
+                    try
+                    {
+                        capacity += long.Parse(baseObj.Properties["Capacity"].Value.ToString());
+                    }
+                    catch
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return (int)(capacity >> 20);
         }
 
     }
---
+++ Perfmon/ProcessMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Perfmon
+{
+    internal class ProcessMonitor
+    {
+        private string _processName;
+        private Process _process;
+        private int _pid;
+        private PerformanceCounter _cpuProc;
+        private PerformanceCounter _vRamUsed;
+        private PerformanceCounter _phyRamUsed;
+        public ProcessMonitor(int pid) 
+        {
+            _process = System.Diagnostics.Process.GetProcessById(pid);
+            _pid = pid;
+            _processName = _process.ProcessName;
+
+            _cpuProc = new PerformanceCounter();
+            _vRamUsed = new PerformanceCounter();
+            _phyRamUsed = new PerformanceCounter();
+        }
+    }
+}
