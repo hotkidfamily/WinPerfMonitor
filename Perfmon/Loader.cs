95d4848db47c97cbae51cf07863a6a084145c5c8
yanli <yanli@yy.com>
2023-10-10 11:00:59 +0800

    add dpi adjust

--- Perfmon/Loader.cs
+++ Perfmon/Loader.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+
             MainForm mainForm = new();
             mainForm.ShowDialog();
         }
