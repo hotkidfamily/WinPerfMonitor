c3309eb6ff1340be26cb62e73574c79832a77806
yanli <yanli@yy.com>
2023-10-18 10:26:24 +0800

    add indicator

--- Perfmon/MainForm.cs
+++ Perfmon/MainForm.cs
@@ -115,6 +115,7 @@ namespace PerfMonitor
             _phyMemTotal = GetPhisicalMemory();
             Task.Run(QuerySystemInfo);
             _ = RefreshListView();
+            labelCpuAndMem.Text = "loading...";
         }
 
 
